cac8d0472e41a294f4ff457b0b1a76904477c80e
Ben <robinsonbena@gmail.com>
2022-05-03 17:17:19 -0700

    created two articles, save them to databse, and added loop to articles controller#index html.erb file to display the title of each article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,5 +2,6 @@ class ArticlesController < ApplicationController
   
   #When an action does not specify a view, RoR automatically renders its corresponding view 
   def index
+    @articles = Article.all
   end
 end
