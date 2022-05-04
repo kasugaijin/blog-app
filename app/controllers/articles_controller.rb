238be9fb1a3c1c0c4e619493d1895c778f1338a3
Ben <robinsonbena@gmail.com>
2022-05-03 20:43:30 -0700

    created new.html.erb view file and added form_with helper

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,4 +8,21 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  # instantiates a new article but does not save it. This is used in its corresponding view file.
+  def new
+    @article = Article.new
+  end
+
+  # instantiates new article with values. The redirect to causes browser to make request for new page.
+  # render is used if save fails and allows instance variables to remain
+  def create
+    @article = Article.new(title: '...', body: '...')
+
+    if @article.save
+      redirect_to @article
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 end
