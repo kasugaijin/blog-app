d77347acc0c53e1ace34dbfb1277c77e3cadca68
Ben <robinsonbena@gmail.com>
2022-05-06 13:27:05 -0700

    added comment delete and dependent: :destroy to Articles controller to allow deletion of article and comments!

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,9 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-import * as bootstrap from "bootstrap"
+
+// import * as bootstrap from "bootstrap" 
+// this line was stopping the 'delete' button from working
 
 import Rails from "@rails/ujs";
 Rails.start();
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ApplicationRecord
-  has_many :comments
+  has_many :comments, dependent: :destroy
 
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
@@ -8,3 +8,4 @@ end
 # has_many :comments tells rails that one article can be associated with many comments in the db
 # Form validation is defined in the Article class
 # title and body attributes are defined by Active Record based on table columns, so not needed here
+# dependent: :destroy is what enables you to delete an article and all of its comments with articles#destroy
